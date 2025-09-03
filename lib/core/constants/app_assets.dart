3d6adef13d28f50bbc1876584b8f194edc74aeb7
RaghdaHany <raghdahany550@gmail.com>
2025-09-03 17:56:59 +0300

    app structure and constants

---
+++ lib/core/constants/app_assets.dart
@@ -0,0 +1,14 @@
+const String imagesPath = 'assets/images';
+const String iconsPath = 'assets/icons';
+
+class AppAssets {
+
+  static const String logoSVG = '$imagesPath/logo.svg';
+  static const String logoTransparentSVG = '$imagesPath/logo_transparent.svg';
+
+  static const String lockSVG = '$imagesPath/lock.svg';
+  static const String emailSVG = '$imagesPath/sms.svg';
+  static const String userSVG = '$imagesPath/user.svg';
+
+
+}
